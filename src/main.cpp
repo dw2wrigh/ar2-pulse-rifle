04e862ff5b60b4337f478d0d775b5795fb25796e
Derek Wright <derek.wright@uwaterloo.ca>
2022-10-09 10:12:47 -0400

    Compiles basic example

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,33 @@
 #include <Arduino.h>
+#include <SD.h>            // need to include the SD library
+#define SD_ChipSelectPin 4 // using digital pin 4 on arduino nano 328, can use other pins
+#include <TMRpcm.h>        // also need to include this library...
+#include <SPI.h>
 
-void setup() {
-  // put your setup code here, to run once:
+TMRpcm tmrpcm; // Create an object for use in this sketch
+
+void setup()
+{
+
+  tmrpcm.speakerPin = 9; // 5,6,11 or 46 on Mega, 9 on Uno, Nano, etc
+
+  Serial.begin(9600);
+  if (!SD.begin(SD_ChipSelectPin))
+  { // see if the card is present and can be initialized:
+    Serial.println("SD fail");
+    return; // don't do anything more if not
+  }
+  tmrpcm.play("music"); // the sound file "music" will play each time the arduino powers up, or is reset
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+
+  if (Serial.available())
+  {
+    if (Serial.read() == 'p')
+    { // send the letter p over the serial monitor to start playback
+      tmrpcm.play("music");
+    }
+  }
 }
