e9653832c1a770e84ef9dc27dcf9fce66898811e
Derek Wright <derek.wright@uwaterloo.ca>
2022-10-12 15:06:07 -0400

    Got audio working

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,57 @@
 #include <Arduino.h>
 #include <SD.h>            // need to include the SD library
-#define SD_ChipSelectPin 4 // using digital pin 4 on arduino nano 328, can use other pins
 #include <TMRpcm.h>        // also need to include this library...
 #include <SPI.h>
+#include <ButtonDebounce.h>
+
+#define SD_CARD_CS 4
+#define SPEAKER_OUT 9
+#define TRIGGER1_IN 1
+#define TRIGGER2_IN 2
+#define TRIGGER3_IN 3
 
 TMRpcm tmrpcm; // Create an object for use in this sketch
+boolean toggle = false;
+// ButtonDebounce button(10, 250);
 
 void setup()
 {
 
-  tmrpcm.speakerPin = 9; // 5,6,11 or 46 on Mega, 9 on Uno, Nano, etc
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, INPUT_PULLUP); // Active Low
+  pinMode(LED_BUILTIN, INPUT_PULLUP); // Active Low
+  pinMode(LED_BUILTIN, INPUT_PULLUP); // Active Low
+  tmrpcm.speakerPin = SPEAKER_OUT;
+  //tmrpcm.setVolume(4);
 
   Serial.begin(9600);
-  if (!SD.begin(SD_ChipSelectPin))
+  if (!SD.begin(SD_CARD_CS))
   { // see if the card is present and can be initialized:
     Serial.println("SD fail");
     return; // don't do anything more if not
   }
-  tmrpcm.play("music"); // the sound file "music" will play each time the arduino powers up, or is reset
+  tmrpcm.play("test2.wav"); // the sound file "music" will play each time the arduino powers up, or is reset
+  digitalWrite(LED_BUILTIN, toggle);
 }
 
 void loop()
 {
-
-  if (Serial.available())
-  {
-    if (Serial.read() == 'p')
-    { // send the letter p over the serial monitor to start playback
-      tmrpcm.play("music");
-    }
-  }
+  // delay(1000);
+  // if (Serial.available())
+  // {
+  //   if (Serial.read() == 'p')
+  //   { // send the letter p over the serial monitor to start playback
+  //     tmrpcm.play("music");
+  //   }
+  // }
+  // toggle = !toggle;
+  // digitalWrite(LED_BUILTIN, toggle);
+  // tone(9, 440, 500);
 }
+
+// void loop() {
+//   button.update();
+//   if(button.state() == HIGH){
+//     Serial.println("Clicked");
+//   }
+// }
